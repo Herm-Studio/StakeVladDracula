837525cc7d995bf89f54bd391c47d7d4ea6579ab
ZhiHang Li <johann_lai@outlook.com>
2024-02-26 15:29:17 +0000

    feat: remove all custome x- headers

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,4 +1,4 @@
-import type { NextRequest } from 'next/server'
+import type { NextRequest, NextFetchEvent } from 'next/server'
 
 export const config = {
   runtime: 'edge'
@@ -8,47 +8,36 @@ export default async function handler(req: NextRequest) {
   console.log('Original Request Headers:', JSON.stringify(Object.fromEntries(req.headers)));
 
   const url = new URL(req.url);
-  
   url.protocol = 'https:';
   url.host = 'api.openai.com';
   url.port = '';
 
-  const headers = new Headers(req.headers)
-  headers.delete('x-vercel-proxied-for')
-  headers.delete('x-forwarded-for');
-  headers.delete('x-forwarded-host');
-  headers.delete('x-real-ip');
-  headers.delete('x-vercel-forwarded-for');
-  headers.delete('x-vercel-deployment-url');
-  headers.delete('X-Vercel-Deployment-Url');
-  headers.delete('X-Vercel-Edge-Region');
-  headers.delete('X-Vercel-Id');
-  headers.delete('X-Vercel-Ip-City');
-  headers.delete('X-Vercel-Ip-Country');
-  headers.delete('X-Vercel-Ip-Country-Region');
-  headers.delete('X-Vercel-Ip-Latitude');
-  headers.delete('X-Vercel-Ip-Longitude');
-  headers.delete('X-Vercel-Ip-Timezone');
-  headers.delete('X-Vercel-Sc-Basepath');
-  headers.delete('X-Vercel-Sc-Host');
-  headers.delete('X-Amzn-Trace-Id');
-  headers.delete('X-Invoke-Output');
-  headers.delete('X-Invoke-Path');
-  headers.delete('X-Invoke-Query');
-  headers.delete('X-Middleware-Invoke');
-  headers.delete('X-Middleware-Subrequest');
+  const headers = new Headers(req.headers);
+
+  headers.forEach((_, key) => {
+    if (key.startsWith('x-') || key.startsWith('X-')) {
+      headers.delete(key);
+    }
+  });
 
   console.log("url.toString()", url.toString());
-  
-  const response = await fetch(
-    url.toString(),
-    {
-      method: req.method,
-      headers: headers,
-      body: req.body,
-      signal: req.signal,
-    }
-  )
 
-  return response;
+  try {
+    const { method, body, signal } = req;
+    const response = await fetch(
+      url.toString(),
+      {
+        method,
+        headers,
+        body,
+        signal,
+      }
+    );
+
+    return response;
+  } catch (error) {
+    console.error('Error fetching the proxied request:', error);
+    // 对于网络错误或请求无法完成的情况，返回500
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }
